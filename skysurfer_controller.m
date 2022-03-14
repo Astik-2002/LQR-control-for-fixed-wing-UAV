29b5c9ac51e3f0eab17bb3817bf5efafd4f133f6
Astik-2002 <101447736+Astik-2002@users.noreply.github.com>
2022-03-14 17:50:14 +0530

    Update skysurfer_controller.m

--- skysurfer_controller.m
+++ skysurfer_controller.m
@@ -26,9 +26,21 @@ C = eye(4);
 D = [0;0;0;0];
 sys = ss(A,B,C,D);
 K_long = lqr(A,B,q,R);
-%N = rscale(A,B,C,D,K_long); 
-%disp(K_long)
-%K1 = [-2.3095   -1.0631   -1.9550  -43.9565];
-%K2 = [0.2681    2.2180    0.0000    0.0921];
 %Lateral_Directional Controller
+%X' = AX+BU; Y = X
+% A_lat = [Y_beta   Y_phi    Y_p    Y_r;
+%           L_beta   L_phi    L_p    L_r;
+%           N_beta   N_phi    N_p    N_r;
+%           0     1      0        0];
+% B_lat = [Y_a Y_r; 
+%           L_a L_r; 
+%           N_a N_r;
+%           0   0];
+A1 = [-0.127 -0.162 -0.0775 1.71;0 0 1 0;29.0 2.71 -8.7 8.55;-20.2 1.46 -2.33 -2.06];
+B1 = [1.72 -7.11;0 0;-327 -28.8;-46.7 81.0];
+q1 = eye(4);
+r1 = 0.2.*eye(2);
+c1 = eye(4);
+d1 = 0;
+k_lat = lqr(A1,B1,q1,r1);
 
